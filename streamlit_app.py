1965862d47ad5cfe5cca00e4ac30382a0a72ebf3
Yazid Irfan <121963204+ponpesassholihah@users.noreply.github.com>
2024-06-19 23:12:20 +0700

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import subprocess
+
+# Membaca dan menampilkan file HTML
+with open('index.html', 'r') as file:
+    html_content = file.read()
+
+st.markdown(html_content, unsafe_allow_html=True)
+
+# Menjalankan script Perl dan menangkap outputnya
+result = subprocess.run(['perl', 'diagnosis.pl'], capture_output=True, text=True)
+
+# Menampilkan output dari script Perl di Streamlit
+st.text(result.stdout)
