8d34ffbadc20811f89d6eaff07f9ec6d7a8523d3
Yazid Irfan <121963204+ponpesassholihah@users.noreply.github.com>
2024-06-19 23:21:59 +0700

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,13 +2,13 @@ import streamlit as st
 import subprocess
 
 # Membaca dan menampilkan file HTML
-with open('index.html', 'r') as file:
+with open('index.html') as file:
     html_content = file.read()
 
 st.markdown(html_content, unsafe_allow_html=True)
 
 # Menjalankan script Perl dan menangkap outputnya
-result = subprocess.run(['diagnosa', 'diagnosis.pl'], capture_output=True, text=True)
+result = subprocess.run(['diagnosis.pl'], capture_output=True, text=True)
 
 # Menampilkan output dari script Perl di Streamlit
 st.text(result.stdout)
