345900a2c2a98b361fa61fa096df29d9a3e0ec91
Yazid Irfan <121963204+ponpesassholihah@users.noreply.github.com>
2024-06-19 23:42:20 +0700

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,4 +8,10 @@ with open('index.html', 'r') as file:
 st.markdown(html_content, unsafe_allow_html=True)
 
 # Menjalankan script Perl dan menangkap outputnya
-result = subprocess.run(['perl', 'diagnosis.pl'], capture_output=True, text=True)
+try:
+    result = subprocess.run(['perl', 'diagnosis.pl'], capture_output=True, text=True, check=True)
+    st.text(result.stdout)
+except subprocess.CalledProcessError as e:
+    st.error(f"Error executing Perl script: {e}")
+    st.error(f"Standard Output: {e.stdout}")
+    st.error(f"Standard Error: {e.stderr}")
